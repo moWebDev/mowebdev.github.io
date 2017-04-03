0debf92ad3f31fd1eb7a39e0b0d8a0bb797d3ee8
Mohammed M Rahman <mmrahman14@hotmail.com>
2017-04-03 18:04:44 +0100

    Open and close menu mobile on scroll
    main.js has extra script in order for the menu close when a item (<li
    ><a>) has been clicked

--- js/main.js
+++ js/main.js
@@ -2,5 +2,9 @@ $(document).ready(function(){
 	$('.mobile-nav-toggle').click(function(){
 		$('.mobile-nav').slideToggle('is-open');
 		$(this).toggleClass('open');
+	});
+	$('.mobile-nav li a').click(function(){
+		$('.mobile-nav').slideToggle('is-open');
+		$('.mobile-nav-toggle.open').removeClass('open');
 	})
 });
